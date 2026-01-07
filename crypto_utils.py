e5d4493c7b2d5b4d7c80ba477930a76e623a6ada
FatimaZ-tech <fatimazakir1312@gmail.com>
2026-01-07 23:14:23 +0500

    Create crypto_utils.py

---
+++ crypto_utils.py
@@ -0,0 +1,12 @@
+import base64
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+def derive_key(password: str, salt: bytes):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100_000,
+    )
+    return base64.urlsafe_b64encode(kdf.derive(password.encode()))
